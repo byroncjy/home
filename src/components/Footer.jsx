552c7f9cc78cb894d2918f9e1d63ce24cd591459
byroncjy <byroncjy@gmail.com>
2024-04-14 22:43:59 -0400

    Basic info changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = (props) => {
     <footer style={bgStyle} className="mt-auto py-5 text-center ">
       <Container>
         {props.children}
-        <i className="fas fa-code" /> with <i className="fas fa-heart" /> by{" "}
+        {/* <i className="fas fa-code" /> with <i className="fas fa-heart" /> by{" "}
         <a
           rel="noopener"
           href="https://github.com/hashirshoaeb"
@@ -17,11 +17,10 @@ const Footer = (props) => {
             Hashir Shoaib
           </span>
         </a>{" "}
-        using <i className="fab fa-react" />
+        using <i className="fab fa-react" /> */}
         <p>
           <small className="text-muted">
-            Project code is open source. Feel free to fork and make your own
-            version.
+            Built upon Hashir Shoaib's fantastic template.
           </small>
         </p>
       </Container>
